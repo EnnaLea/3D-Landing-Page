54e174ffe407843672796b99cb357a0bf1d6529e
EnnaLea <49825624+leaaliman@users.noreply.github.com>
2023-01-28 21:51:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const boxes = document.querySelectorAll("box");
+const boxes = document.querySelectorAll(".box");
 
 // variable to count the boxes in the array
 let current = 1;
@@ -6,7 +6,7 @@ let current = 1;
 // to move and rotate the boxes
 let interval = setInterval(() => {
     boxes.forEach((box) => {
-        if (box.classList[1].split("-")[1] * 1 == current) {
+        if (box.classList[1].split("-")[1] * 1 === current) {
             box.classList.add("active");
         } else {
         }
